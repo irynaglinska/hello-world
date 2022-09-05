3c0f1aef0d887e0f8644fe05d6f38700d68cdea5
irynaglinska <irasimak95@gmail.com>
2022-09-05 19:28:38 +0000

    add

---
+++ js/main.js
@@ -0,0 +1,3 @@
+var headingElement = document.getElementById("main-heading");  console.log(headingElement.innerHTML);
+var newHeadingText = prompt("Введите новый заголовок:");
+headingElement.innerHTML = newHeadingText;
